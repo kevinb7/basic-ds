5e437bf2a0f255fc3880e2eed99806b3e5696861
Kevin Barabash <kevinb7@gmail.com>
2014-12-01 20:24:46 -0700

    removed gulp file, updated travis to use npm test

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require("gulp");
-var mochaPhantomJS = require("gulp-mocha-phantomjs");
-
-gulp.task("test", function () {
-    gulp.src('test/runner.html')
-        .pipe(mochaPhantomJS({reporter: 'dot'}));
-});
