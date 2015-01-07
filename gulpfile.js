11b88a3bb3d69b7df6e21321ec99cf6bd0896f48
Kevin Barabash <kevinb7@gmail.com>
2015-01-07 10:37:49 -0700

    migrate to gulp for more flexible build system

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var to5 = require('gulp-6to5');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('compile', function () {
+    return gulp.src('src/**/*.js')
+        .pipe(to5())
+        .pipe(gulp.dest('lib'));
+});
+
+gulp.task('bundle', ['compile'], function () {
+    return browserify({ standalone: "basic" })
+        .require("./lib/basic.js", { entry: true })
+        .bundle()
+        .pipe(source('basic.js'))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', ['bundle']);
